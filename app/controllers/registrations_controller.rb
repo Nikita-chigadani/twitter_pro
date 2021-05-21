eb8e130962b7db377b7e85fad434e1b42518f1c7
Nikita C <nikita149156@gmail.com>
2021-05-21 11:43:41 +0530

    registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
     private
 
     def sign_up_params
-        params.require(:user).permit(:name, :usename, :email, :password_confirmation)
+        params.require(:user).permit(:name, :usename, :email, :password,  :password_confirmation)
     end
 
     def acount_update_params
-        params.require(:user).permit(:name, :usename, :email, :password_confirmation, :curent_password)
+        params.require(:user).permit(:name, :usename, :email,:password, :password_confirmation, :curent_password)
     end
 
     
