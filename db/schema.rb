3ffb9982c0806943912f787249ab8440efd30f08
Nikita C <nikita149156@gmail.com>
2021-05-21 16:03:11 +0530

    exam

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,18 @@ ActiveRecord::Schema.define(version: 2021_05_19_102738) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "usename"
-    t.index "\"username\"", name: "index_users_on_username", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["usename"], name: "index_users_on_usename", unique: true
   end
 
 end
