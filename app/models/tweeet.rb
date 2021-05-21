e2b79faadc84bc14a41d805a8e07d0387a1dcbc0
Nikita C <nikita149156@gmail.com>
2021-05-21 16:40:56 +0530

    testing

--- app/models/tweeet.rb
+++ app/models/tweeet.rb
@@ -1,3 +1,5 @@
 class Tweeet < ApplicationRecord
+    validates :tweet_about_it, presence: true,
+                                length: {minimum: 5}
     belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_102738) do
+ActiveRecord::Schema.define(version: 2021_05_21_110509) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_102738) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "usename"
+    t.string "DOB"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["usename"], name: "index_users_on_usename", unique: true
