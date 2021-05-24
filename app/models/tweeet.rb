7a818106b17d74f961bd7a13a4fafe542a5f0eb5
Nikita C <nikita149156@gmail.com>
2021-05-24 14:44:33 +0530

    changes

--- app/models/tweeet.rb
+++ app/models/tweeet.rb
@@ -1,5 +1,5 @@
 class Tweeet < ApplicationRecord
-    validates :tweet_about_it, presence: true,
-                                length: {minimum: 5}
-    belongs_to :user
+     validates :tweeet, presence: true,
+                                 length: {minimum: 5}
+     belongs_to :user
 end
